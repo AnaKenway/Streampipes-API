6b0ec97c28a4bdb7e5ef83b28fa5dd6c682667bc
AnaKenway <ancemilenkovic@gmail.com>
2023-01-09 23:06:12 +0100

    Changed port of InfluxDB to 8085

--- Streampipes-API/Services/InfluxDBService.cs
+++ Streampipes-API/Services/InfluxDBService.cs
@@ -16,23 +16,18 @@ namespace Streampipes_API.Services
 
         public void Write(Action<WriteApi> action)
         {
-            //using var client = InfluxDBClientFactory.Create("http://localhost:8086", _token);
-            using var client = InfluxDBClientFactory.Create("http://host.docker.internal:8086", _token);
+            //using var client = InfluxDBClientFactory.Create("http://localhost:8085", _token);
+            using var client = InfluxDBClientFactory.Create("http://host.docker.internal:8085", _token);
             using var write = client.GetWriteApi();
             action(write);
         }
 
         public async Task<T> QueryAsync<T>(Func<QueryApi, Task<T>> action)
         {
-            //using var client = InfluxDBClientFactory.Create("http://localhost:8086", _token);
-            using var client = InfluxDBClientFactory.Create("http://host.docker.internal:8086", _token);
+            //using var client = InfluxDBClientFactory.Create("http://localhost:8085", _token);
+            using var client = InfluxDBClientFactory.Create("http://host.docker.internal:8085", _token);
             var query = client.GetQueryApi();
             return await action(query);
         }
     }
 }
-//Timestamp = long.Parse(record.GetTime().Value.ToString()),
-//                            InfluxId = record.Values["influx-id"].ToString(),
-//                            Temperature = double.Parse(record.Values["temperature"].ToString()),
-//                            Pressure = double.Parse(record.Values["pressure"].ToString())
-//write.WriteRecord($"monitoringData,monitoring=test-monitoring influx-id={data.InfluxId},temperature={data.Temperature},pressure={data.Pressure} {data.Timestamp}", WritePrecision.Ns, "test-bucket", "organization");
